cecdcadd461b658b5e66dd6b0115545f2d8da62f
naglicmoc <dvd.filmas@gmail.com>
2025-03-06 16:08:30 +0200

    Fuknciju pati baze

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+import random
 
 
 def print_list_col(arr):
@@ -24,11 +26,34 @@ print_list_col(students_grades)
 
 print_list_col(students_grades)
 
+def say_hi():#nieko nepriima, nieko negrazina
+    print("hi")
 
+say_hi()
 
+def say_hi_to(name):#priima viena kintamaji, nieko negrazina
+    print(f"hi, {name}")
 
+say_hi_to("Jonas")
+say_hi_to("Rolandas")
 
+vardas = "Julija"
+say_hi_to(vardas)
+vardas = "Jonas"
+say_hi_to(vardas)
 
+def sim_pi():#nieko nepriima, bet GRAZINA reiksme I TEN kur buvo iskviesta
+    return 3.1415
 
+pi_val = sim_pi()
+print(pi_val)
+print(sim_pi())
+print(math.pi)
 
+def make_initials(name, surname):# priima DU kintamuosius ir grazina reiksme
+    return (name[0] + surname[0]).upper()
 
+pavarde = "Marozaitis"
+initials = make_initials(vardas, pavarde)
+
+print(initials)
